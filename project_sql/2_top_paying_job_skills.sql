e11fdbafea155b37a58c0024798f56c517c4eabf
Jack Ford <jackford282@gmail.com>
2025-03-09 19:25:05 -0400

    added readme and images

---
+++ project_sql/2_top_paying_job_skills.sql
@@ -0,0 +1,361 @@
+/*
+Question: What skills are required for the top-paying data analyst jobs?
+- Use the top 10 highest-paying Data Analyst jobs from first query
+- Add the specific skills required for these roles
+- Why? It provides a detailed look at which high-paying jobs demand certain skills, 
+    helping job seekers understand which skills to develop that align with top salaries
+*/
+
+WITH top_paying_jobs AS(
+    SELECT
+        job_id,
+        job_title,
+        salary_year_avg,
+        name AS company_name
+    FROM
+        job_postings_fact
+    LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+    WHERE
+        job_title_short = 'Data Analyst' AND 
+        job_location IN ('Anywhere', 'New York, NY') AND
+        salary_year_avg IS NOT NULL
+    ORDER BY
+        salary_year_avg DESC
+    LIMIT 10
+)
+
+SELECT
+    top_paying_jobs.*,
+    skills
+FROM top_paying_jobs
+INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY
+    salary_year_avg DESC
+
+/*
+Here's the breakdown of the most demanded skills for data analysts in 2023, based on job postings:
+SQL is leading with a bild count of 8.
+Python follows closely with a bold count of 7.
+Tableau is also highly sought after, with a bold count of 6.
+Other skills like R, Snowflake, Pandas, and Excel show varying degrees of demand.
+
+[
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "sql"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "python"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "r"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "azure"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "databricks"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "aws"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "pandas"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "pyspark"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "jupyter"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "excel"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "tableau"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "power bi"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "powerpoint"
+  },
+  {
+    "job_id": 339646,
+    "job_title": "Data Sector Analyst - Hedge Fund in Midtown",
+    "salary_year_avg": "240000.0",
+    "company_name": "Coda Search│Staffing",
+    "skills": "sql"
+  },
+  {
+    "job_id": 339646,
+    "job_title": "Data Sector Analyst - Hedge Fund in Midtown",
+    "salary_year_avg": "240000.0",
+    "company_name": "Coda Search│Staffing",
+    "skills": "python"
+  },
+  {
+    "job_id": 339646,
+    "job_title": "Data Sector Analyst - Hedge Fund in Midtown",
+    "salary_year_avg": "240000.0",
+    "company_name": "Coda Search│Staffing",
+    "skills": "pandas"
+  },
+  {
+    "job_id": 1713491,
+    "job_title": "Investigations and Insights Lead Data Analyst - USDS",
+    "salary_year_avg": "239777.5",
+    "company_name": "TikTok",
+    "skills": "sql"
+  },
+  {
+    "job_id": 1713491,
+    "job_title": "Investigations and Insights Lead Data Analyst - USDS",
+    "salary_year_avg": "239777.5",
+    "company_name": "TikTok",
+    "skills": "python"
+  },
+  {
+    "job_id": 1713491,
+    "job_title": "Investigations and Insights Lead Data Analyst - USDS",
+    "salary_year_avg": "239777.5",
+    "company_name": "TikTok",
+    "skills": "r"
+  },
+  {
+    "job_id": 1713491,
+    "job_title": "Investigations and Insights Lead Data Analyst - USDS",
+    "salary_year_avg": "239777.5",
+    "company_name": "TikTok",
+    "skills": "express"
+  },
+  {
+    "job_id": 841064,
+    "job_title": "Investigations and Insights Lead Data Analyst - USDS",
+    "salary_year_avg": "239777.5",
+    "company_name": "TikTok",
+    "skills": "sql"
+  },
+  {
+    "job_id": 841064,
+    "job_title": "Investigations and Insights Lead Data Analyst - USDS",
+    "salary_year_avg": "239777.5",
+    "company_name": "TikTok",
+    "skills": "python"
+  },
+  {
+    "job_id": 841064,
+    "job_title": "Investigations and Insights Lead Data Analyst - USDS",
+    "salary_year_avg": "239777.5",
+    "company_name": "TikTok",
+    "skills": "r"
+  },
+  {
+    "job_id": 841064,
+    "job_title": "Investigations and Insights Lead Data Analyst - USDS",
+    "salary_year_avg": "239777.5",
+    "company_name": "TikTok",
+    "skills": "express"
+  },
+  {
+    "job_id": 99305,
+    "job_title": "Data Analyst, Marketing",
+    "salary_year_avg": "232423.0",
+    "company_name": "Pinterest Job Advertisements",
+    "skills": "sql"
+  },
+  {
+    "job_id": 99305,
+    "job_title": "Data Analyst, Marketing",
+    "salary_year_avg": "232423.0",
+    "company_name": "Pinterest Job Advertisements",
+    "skills": "python"
+  },
+  {
+    "job_id": 99305,
+    "job_title": "Data Analyst, Marketing",
+    "salary_year_avg": "232423.0",
+    "company_name": "Pinterest Job Advertisements",
+    "skills": "r"
+  },
+  {
+    "job_id": 99305,
+    "job_title": "Data Analyst, Marketing",
+    "salary_year_avg": "232423.0",
+    "company_name": "Pinterest Job Advertisements",
+    "skills": "hadoop"
+  },
+  {
+    "job_id": 99305,
+    "job_title": "Data Analyst, Marketing",
+    "salary_year_avg": "232423.0",
+    "company_name": "Pinterest Job Advertisements",
+    "skills": "tableau"
+  },
+  {
+    "job_id": 204500,
+    "job_title": "Reference Data Analyst",
+    "salary_year_avg": "225000.0",
+    "company_name": "Selby Jennings",
+    "skills": "sql"
+  },
+  {
+    "job_id": 204500,
+    "job_title": "Reference Data Analyst",
+    "salary_year_avg": "225000.0",
+    "company_name": "Selby Jennings",
+    "skills": "python"
+  },
+  {
+    "job_id": 1021647,
+    "job_title": "Data Analyst (Hybrid/Remote)",
+    "salary_year_avg": "217000.0",
+    "company_name": "Uclahealthcareers",
+    "skills": "sql"
+  },
+  {
+    "job_id": 1021647,
+    "job_title": "Data Analyst (Hybrid/Remote)",
+    "salary_year_avg": "217000.0",
+    "company_name": "Uclahealthcareers",
+    "skills": "crystal"
+  },
+  {
+    "job_id": 1021647,
+    "job_title": "Data Analyst (Hybrid/Remote)",
+    "salary_year_avg": "217000.0",
+    "company_name": "Uclahealthcareers",
+    "skills": "oracle"
+  },
+  {
+    "job_id": 1021647,
+    "job_title": "Data Analyst (Hybrid/Remote)",
+    "salary_year_avg": "217000.0",
+    "company_name": "Uclahealthcareers",
+    "skills": "tableau"
+  },
+  {
+    "job_id": 1021647,
+    "job_title": "Data Analyst (Hybrid/Remote)",
+    "salary_year_avg": "217000.0",
+    "company_name": "Uclahealthcareers",
+    "skills": "flow"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "salary_year_avg": "205000.0",
+    "company_name": "SmartAsset",
+    "skills": "sql"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "salary_year_avg": "205000.0",
+    "company_name": "SmartAsset",
+    "skills": "python"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "salary_year_avg": "205000.0",
+    "company_name": "SmartAsset",
+    "skills": "go"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "salary_year_avg": "205000.0",
+    "company_name": "SmartAsset",
+    "skills": "snowflake"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "salary_year_avg": "205000.0",
+    "company_name": "SmartAsset",
+    "skills": "pandas"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "salary_year_avg": "205000.0",
+    "company_name": "SmartAsset",
+    "skills": "numpy"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "salary_year_avg": "205000.0",
+    "company_name": "SmartAsset",
+    "skills": "excel"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "salary_year_avg": "205000.0",
+    "company_name": "SmartAsset",
+    "skills": "tableau"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "salary_year_avg": "205000.0",
+    "company_name": "SmartAsset",
+    "skills": "gitlab"
+  }
+]
+
+*/
